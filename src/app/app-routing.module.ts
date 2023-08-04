60594366c3c35e675af2e0a112f11b4045cf352b
Juan Camilo Villacob <jucaviza6@gmail.com>
2023-08-04 17:42:27 -0500

    fomrulario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CotizarComponent } from './components/cotizar/cotizar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home'},
+  { path: 'home', component: HomeComponent},
+  { path: 'cotizar', component: CotizarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { ClientesComponent } from './components/home/clientes/clientes.component';
+import { CotizarComponent } from './components/cotizar/cotizar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    ClientesComponent
+    ClientesComponent,
+    CotizarComponent
   ],
   imports: [
     BrowserModule,
