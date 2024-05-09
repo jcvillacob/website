14cb97a6cf447d38882d748b50489349e329f32b
jvillacob <jvillacob@coorditanques.com>
2024-05-09 17:26:17 -0500

    pqrs

--- src/app/components/pqrs/pqrs.component.ts
+++ src/app/components/pqrs/pqrs.component.ts
@@ -5,73 +5,80 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-pqrs',
   templateUrl: './pqrs.component.html',
-  styleUrls: ['./pqrs.component.css']
+  styleUrls: ['./pqrs.component.css'],
 })
 export class PqrsComponent implements OnInit {
+  archivoSeleccionado: File | null = null;
+  uploadedFiles: File[] = [];
+  descripciones: any = {
+    Peticion: 'Una petición es una solicitud formal para obtener un beneficio o servicio específico.',
+    Queja: 'Una queja se presenta cuando el servicio o producto no cumple con las expectativas del cliente.',
+    Reclamo: 'Un reclamo se hace para exigir una solución debido a una inconformidad con el producto o servicio recibido.',
+    Sugerencia: 'Una sugerencia es una propuesta para mejorar algún aspecto del servicio o producto ofrecido.'
+  };
 
-  /* Para crear la solicitud */
+  /* Para crear la PQRS */
   empresa!: string;
   nit!: string;
   nombre!: string;
   email!: string;
-  origen!: string;
-  direccionOrigen!: string;
-  destino!: string;
-  direccionDestino!: string;
-  tipoVh!: string;
-  producto!: string;
-  observaciones!: string;
+  cargo!: string;
+  telefono!: string;
+  tipoPqrs!: string;
+  descripcion!: string;
 
-  constructor(private formulariosService: FormulariosService) {
-
-  }
-
+  constructor(private formulariosService: FormulariosService) {}
 
   ngOnInit(): void {
-      this.tipoVh = 'TM';
+    this.tipoPqrs = 'Peticion';
+  }
+
+  onFileSelected(event: any): void {
+    this.archivoSeleccionado = event.target.files[0];
+    if (this.archivoSeleccionado && this.archivoSeleccionado.size > 5242880) {
+      Swal.fire(
+        'Archivo demasiado grande',
+        'El archivo no debe exceder los 5 MB',
+        'error'
+      );
+      this.archivoSeleccionado = null;
+      event.target.value = '';
+    } else {
+      if (this.archivoSeleccionado) {
+        this.uploadedFiles.push(this.archivoSeleccionado);
+        this.archivoSeleccionado = null;
+        (document.getElementById('fileUpload') as HTMLInputElement).value = '';
+      }
+    }
   }
 
   createSolicitudes() {
-    if (!this.empresa || !this.nit || !this.nombre || !this.email || !this.origen || !this.direccionOrigen || !this.destino || !this.direccionDestino || !this.tipoVh || !this.producto || !this.observaciones) {
+    if (
+      !this.empresa ||
+      !this.nit ||
+      !this.nombre ||
+      !this.email ||
+      !this.cargo ||
+      !this.telefono ||
+      !this.tipoPqrs ||
+      !this.descripcion
+    ) {
       Swal.fire(
         'Debes Ingresar Todos los datos',
-        `La cotización No ha sido enviada.`,
+        `La Solicitud No ha sido enviada.`,
         'error'
-      )
-      return
-    };
+      );
+      return;
+    }
     const data = {
       empresa: this.empresa,
       nit: this.nit,
       nombre: this.nombre,
       email: this.email,
-      origen: this.origen,
-      direccionOrigen: this.direccionOrigen,
-      destino: this.destino,
-      direccionDestino: this.direccionDestino,
-      tipoVh: this.tipoVh,
-      producto: this.producto,
-      observacion: this.observaciones
+      cargo: this.cargo,
+      telefono: this.telefono,
+      tipoPqrs: this.tipoPqrs,
+      descripcion: this.descripcion,
     };
-
-    this.formulariosService.createSolicitudes(data).subscribe(data => {
-      this.empresa = '';
-      this.nit = '';
-      this.nombre = '';
-      this.email = '';
-      this.origen = '';
-      this.direccionOrigen = '';
-      this.destino = '';
-      this.direccionDestino = '';
-      this.tipoVh = '';
-      this.producto = '';
-      this.observaciones = '';
-      Swal.fire(
-        'Cotización Enviada',
-        `Pronto nos contactaremos contigo.`,
-        'success'
-      );
-    });
   }
-
 }
